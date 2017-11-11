26db4f9fac9c7421e6c9414ad6424123d732dfa1
Mike Gore <mikegore1000@msn.com>
2017-11-11 15:09:39 +0000

    Initial commit

---
+++ src/GraphProjection/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.ChangeFeedProcessor;
+
+namespace GraphProjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
